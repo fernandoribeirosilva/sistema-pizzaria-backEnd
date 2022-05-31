1cfccbd10e5e42f26253b4822b89a9ac4073854e
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-05-31 10:41:15 -0300

    implementado função de registra o endereço e buscar um endereço espécifico no banco de dados

---
+++ src/services/ClientService.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+import { ClientProps, Address } from '../types/ClientProps';
+
+export const CLientService = {
+   create: async (data: ClientProps) => {
+      return await prisma.client.create({
+         data: {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            adress: {
+               connectOrCreate: {
+                  where: { id: data.addressId },
+                  create: {
+                     street: data.street,
+                     block: data.block,
+                     batch: data.batch,
+                     complement: data.complement,
+                     district: {
+                        connectOrCreate: {
+                           where: { name: data.nameDistrict },
+                           create: {
+                              name: data.nameDistrict
+                           }
+                        }
+                     }
+                  }
+               }
+            },
+         },
+         include: {
+            adress: {
+               include: {
+                  district: true
+               }
+            }
+         }
+      })
+   },
+
+   findBayAddress: async (data: Address) => {
+      return await prisma.address.findMany({
+         where: {
+            AND: [
+               {
+                  street: data.street,
+                  block: data.block,
+                  batch: data.batch,
+               },
+            ]
+         },
+         select: {
+            id: true,
+            street: true,
+            block: true,
+            batch: true,
+            complement: true,
+            districtId: true,
+         }
+      });
+   }
+}
