79e49c1ef86fe55540dd1c844ddd6158d02d27ac
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-06-07 09:39:48 -0300

    implementado a verificação se o campo adm e true ou false para liberar o acesso

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 import JWT from 'jsonwebtoken';
 
 import { AdmService } from '../services/AdmService';
+import { log } from 'console';
 
 dotenv.config();
 
+
 export const Auth = {
    private: async (req: Request, res: Response, next: NextFunction) => {
-      if (!req.query.token && !req.body.token) return res.status(401).json({ notallowed: true });
+      /* if (!req.query.token && !req.body.token) return res.status(401).json({ notallowed: true });
 
       let token = '';
       if (req.query.token) {
@@ -26,6 +28,34 @@ export const Auth = {
 
       if (!user) return res.status(401).json({ notallowed: true });
 
-      next();
+      next(); */
+
+      let success = false;
+
+
+      if (req.headers.authorization) {
+
+         const [authType, token] = req.headers.authorization.split(' ');
+         if (authType === 'Bearer') {
+            try {
+               const { sub } = JWT.verify(token, process.env.JWT_SECRET as string);
+
+               if (sub !== 'true') throw new Error('Invalid token');
+
+               success = true;
+
+
+            } catch (error) {
+               // vai passar direto para a mensagem Não autorizado
+               return res.status(403).json({ error: 'Não autorizado.' });
+            }
+         }
+      }
+
+      if (success) {
+         next();
+      } else {
+         return res.status(403).json({ error: 'Seção expirada.' });
+      }
    }
 }
